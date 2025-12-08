c45e1c6b202cd91fb31911d5b37358998cf56a77
aparnadhiraj12-crypto <aparnadhiraj12@gmail.com>
2025-12-08 23:42:08 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { DesignWorkspace } from './components/DesignWorkspace';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-100">
+      <DesignWorkspace />
+    </div>
+  );
+}
